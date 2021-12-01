b8c4cb28385faf4d7445d5ce88318163a08e674d
Artemiy <rendrom@gmail.com>
2021-12-01 21:01:28 +0200

    Try build with GeoTIFF

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
+// import "ol/ol.css";
+import GeoTIFF from "ol/source/GeoTIFF";
+import Map from "ol/Map";
+import TileLayer from "ol/layer/WebGLTile";
 
-new Map({
-  target: 'map',
+const source = new GeoTIFF({
+  sources: [
+    {
+      url: "https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/2020/S2A_36QWD_20200701_0_L2A/TCI.tif",
+    },
+  ],
+});
+
+const map = new Map({
+  target: "map",
   layers: [
     new TileLayer({
-      source: new XYZ({
-        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      })
-    })
+      source: source,
+    }),
   ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+  view: source.getView(),
 });
--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,28 @@
-import cjs from '@rollup/plugin-commonjs';
-import node from '@rollup/plugin-node-resolve';
-import {terser} from 'rollup-plugin-terser';
+import cjs from "@rollup/plugin-commonjs";
+import node from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'main.js',
+  input: "main.js",
   output: [
-    {file: 'bundle.js', format: 'iife', sourcemap: true}
+    {
+      file: "bundle.js",
+      format: "iife",
+      sourcemap: true,
+      inlineDynamicImports: true,
+    },
   ],
-  plugins: [
-    node(),
-    cjs(),
-    production && terser()
-  ],
-  onwarn: function(warning, superOnWarn) {
+  plugins: [node(), cjs(), production && terser()],
+  onwarn: function (warning, superOnWarn) {
     /*
      * skip certain warnings
      * https://github.com/openlayers/openlayers/issues/10245
      */
-    if (warning.code === 'THIS_IS_UNDEFINED') {
+    if (warning.code === "THIS_IS_UNDEFINED") {
       return;
     }
     superOnWarn(warning);
-  }
+  },
 };
